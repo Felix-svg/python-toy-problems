0bcafedd0f40c3ccf9dabe46f2b6cb2de61810e2
Felix Omondi <136954351+Felix-svg@users.noreply.github.com>
2024-03-15 10:00:55 +0300

    removed the string module

--- code-challenge/challenge3.py
+++ code-challenge/challenge3.py
@@ -2,21 +2,22 @@
 Write a function solution that, given an integer N, returns a string of length N containing as many different lower-case letters ('a'-'z') as possible, in which each letter occurs an equal number of times.
 """
 
-import string
+# import string
 import random
 
+
 def solution(N):
-    # letters = 'abcdefghijklmnopqrstuvwxyz'
-    letters = string.ascii_lowercase
+    letters = "abcdefghijklmnopqrstuvwxyz"
+    # letters = string.ascii_lowercase
     count = N // 26
     remainder = N % 26
-    result = ''.join([letters * count])
+    result = "".join([letters * count])
     result += letters[:remainder]
 
     result_list = list(result)
     random.shuffle(result_list)
 
-    return ''.join(result_list)
+    return "".join(result_list)
 
 
 print(solution(5))
